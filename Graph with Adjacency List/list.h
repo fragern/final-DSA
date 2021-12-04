e8de90f8b9f77fd19ad2371117aa428fcdc8bf85
Nafis <70643415+fragern@users.noreply.github.com>
2021-12-05 06:50:08 +0700

    Create list.h

---
+++ Graph with Adjacency List/list.h
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#ifndef LIST_H
+#define LIST_H
+
+using namespace std;
+
+class List
+{
+public:
+    void push(vector<int> array[], int a, int b);
+    void printGraph(vector<int> array[], int V);
+};
+
+#endif
