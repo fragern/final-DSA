5626b3dafb226ff1ca0bd38366c93a12a71ff37a
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2021-12-05 20:36:47 +0700

    update sikit ehe

--- Graph with Adjacency List/main.cpp
+++ Graph with Adjacency List/main.cpp
@@ -7,20 +7,40 @@ int main()
 {
     List list;
     int temp;
-  
+    int a;
+    int b;
+    int t;
+    string tempo;
+    vector<string> names;
     int V;
     cout<<"How many destination points would you like to add? ";
     cin<<temp;
     V=temp;
+    
+   cout<<"please add labels for each destination points"<<endl;
+    for (int i=0;i<V;i++)
+    {
+        cout<<"Destination Point Number "<<i+1<<": ";
+        getline(cin,tempo);
+        names.push_back(tempo);
+    }
+    
+   cout<<"Directory: "<<endl;
+   for (int j=0;j<V;j++)
+    {
+        cout<<"Destination Point Number "<<j+1<<": "<<names[j]<<endl;
+    }
+        
+    
+    cout<<"Please enter estimated time taken (t) to go from point a to z in the format : a b t"<<endl;
+    cout<<"to stop putting data in type 999 999 999"<<endl;
   
     // Create a graph
     vector<int> arr[V];
 
-    // Add edges and print the graph
-    list.push(arr, 1, 2);
-    list.push(arr, 1, 2);
-    list.push(arr, 2, 3);
-    list.push(arr, 3, 4);
-    list.push(arr, 4, 2);
+    while (a!=999 && b!= 999 && c!= 999)
+    {
+        list.push(arr, a, b, c);
+    }
     list.printGraph(arr, V);
 }
