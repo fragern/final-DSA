681b3f214620e07f4ee8fb188687207203d315be
Nafis <70643415+fragern@users.noreply.github.com>
2021-12-08 09:10:24 +0700

    Changing c into dist
    - changing variable c to dist
    - tidy up the code a bit

--- Graph with Adjacency List/main.cpp
+++ Graph with Adjacency List/main.cpp
@@ -10,38 +10,39 @@ int main()
     int a;
     int b;
     int t;
+    int distance;
     string tempo;
     vector<string> names;
     int V;
-    cout<<"How many destination points would you like to add? ";
-    cin>>temp;
+
+    cout << "How many destination points would you like to add? ";
+    cin >> temp;
     cin.ignore();
-    V=temp;
-    
-   cout<<"please add labels for each destination points"<<endl;
-    for (int i=0;i<V;i++)
+    V = temp;
+
+    cout << "please add labels for each destination points" << endl;
+    for (int i = 0; i < V; i++)
     {
-        cout<<"Destination Point Number "<<i+1<<": ";
-        getline(cin,tempo);
+        cout << "Destination Point Number " << i + 1 << ": ";
+        getline(cin, tempo);
         names.push_back(tempo);
     }
-    
-   cout<<"Directory: "<<endl;
-   for (int j=0;j<V;j++)
+
+    cout << "Directory: " << endl;
+    for (int j = 0; j < V; j++)
     {
-        cout<<"Destination Point Number "<<j+1<<": "<<names[j]<<endl;
+        cout << "Destination Point Number " << j + 1 << ": " << names[j] << endl;
     }
-        
-    
-    cout<<"Please enter estimated time taken (t) to go from point a to z in the format : a b t"<<endl;
-    cout<<"to stop putting data in type 999 999 999"<<endl;
-  
+
+    cout << "Please enter estimated time taken (t) to go from point a to z in the format : a b t" << endl;
+    cout << "to stop putting data in type 999 999 999" << endl;
+
     // Create a graph
     vector<int> arr[V];
 
-    while (a!=999 && b!= 999 && c!= 999)
+    while (a != 999 && b != 999 && distance != 999)
     {
-        list.push(arr, a, b, c);
+        list.push(arr, a, b, distance);
     }
     list.printGraph(arr, V);
 }
