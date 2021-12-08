c884da3b2ede7be296cf78894da45012a447f5d7
Nafis <70643415+fragern@users.noreply.github.com>
2021-12-08 08:52:41 +0700

    Add Distance
    - setting the distance for dijkstra
    
    NOTE: still don't know if this works or not. might need some update later

--- Graph with Adjacency List/list.cpp
+++ Graph with Adjacency List/list.cpp
@@ -3,10 +3,13 @@
 
 using namespace std;
 
-void List::push(vector<int> array[], int a, int b)
+void List::push(vector<int> array[], int a, int b, int dist)
 {
     array[a].push_back(b);
     array[b].push_back(a);
+
+    // setting up the distance of dijkstra
+    this->dist = dist;
 }
 
 void List::printGraph(vector<int> array[], int V)
