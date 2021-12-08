6ababc6855ff3e89d9e0f428523f9ecf7a3f7743
Nafis <70643415+fragern@users.noreply.github.com>
2021-12-08 09:03:35 +0700

    Changing c to dist
    - Changing variable c to dist 
    - Tidy up the code a bit

--- Graph with Adjacency List/list.h
+++ Graph with Adjacency List/list.h
@@ -1,18 +1,48 @@
 #include <bits/stdc++.h>
-#ifndef LIST_H
-#define LIST_H
-
 using namespace std;
 
-class List
-{
-public:
-    void push(vector<int> array[], int a, int b, int dist);
-    void printGraph(vector<int> array[], int V);
+#include "list.h"
 
+int main()
+{
+    List list;
+    int temp;
     int a;
     int b;
-    int dist;
-};
+    int t;
+    int distance;
+    string tempo;
+    vector<string> names;
+    int V;
 
-#endif
+    cout << "How many destination points would you like to add? ";
+    cin >> temp;
+    cin.ignore();
+    V = temp;
+
+    cout << "please add labels for each destination points" << endl;
+    for (int i = 0; i < V; i++)
+    {
+        cout << "Destination Point Number " << i + 1 << ": ";
+        getline(cin, tempo);
+        names.push_back(tempo);
+    }
+
+    cout << "Directory: " << endl;
+    for (int j = 0; j < V; j++)
+    {
+        cout << "Destination Point Number " << j + 1 << ": " << names[j] << endl;
+    }
+
+    cout << "Please enter estimated time taken (t) to go from point a to z in the format : a b t" << endl;
+    cout << "to stop putting data in type 999 999 999" << endl;
+
+    // Create a graph
+    vector<int> arr[V];
+
+    while (a != 999 && b != 999 && distance != 999)
+    {
+        list.push(arr, a, b, distance);
+    }
+    list.printGraph(arr, V);
+}
