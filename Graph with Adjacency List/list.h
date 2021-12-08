50761536b7eb317a285daae370d048a1b80c85ce
Po7a7i <57235501+Po7a7i@users.noreply.github.com>
2021-12-08 12:29:13 +0700

    Update list.h

--- Graph with Adjacency List/list.h
+++ Graph with Adjacency List/list.h
@@ -7,8 +7,8 @@ using namespace std;
 class List
 {
 public:
-    void push(vector<pair<int, int> array[], int a, int b, int time);
-    void printGraph(vector<pair<int, int> array[], int V);
+    void push(vector<pair<int, int>> array[], int a, int b, int time);
+    void printGraph(vector<pair<int, int>> array[], int V);
 
     int a;
     int b;
